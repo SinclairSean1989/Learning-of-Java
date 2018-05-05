12d49a33f83594a24b2881489d1653d8c90c6f94
SinclairSean1989 <SinclairSean1989@users.noreply.github.com>
2018-05-05 15:36:33 +0800

    1

--- day2/boardScanner.java
+++ day2/boardScanner.java
@@ -2,7 +2,9 @@ import java.util.Scanner;
 class boardScanner{
   public static void main(String args[]){
     Scanner sc = new Scanner(System.in);
+    System.out.print("please input number a: ");
     int a = sc.nextInt();
+    System.out.print("please input number b: ");
     int b = sc.nextInt();
     System.out.println("a+b="+(a+b));
   }
