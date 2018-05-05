d656e71a34d7b6c30c410bb793435576a2c5b93b
SinclairSean1989 <SinclairSean1989@users.noreply.github.com>
2018-05-05 15:41:20 +0800

    1

--- day2/boardScanner.java
+++ day2/boardScanner.java
@@ -1,3 +1,8 @@
+
+/**
+键盘录入两个数据，并对这两个数据求和，输出其结果;
+键盘录入两个数据，并对这两个数据求和，输出其结果;
+*/
 import java.util.Scanner;
 class boardScanner{
   public static void main(String args[]){
@@ -7,5 +12,7 @@ class boardScanner{
     System.out.print("please input number b: ");
     int b = sc.nextInt();
     System.out.println("a+b="+(a+b));
+    int c = (a>b) ? a : b;
+    System.out.println("the bigger number is : "+c);
   }
 }
